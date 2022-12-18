501b001484bd5f7d331ca85a48bc284e703440a1
AnneSoOJ <annesophie.ombouajalais@jask-si.com>
2022-12-18 23:12:13 +0100

    CI/CD pipeline

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error(request):
+    return 1 / 0
+
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("lettings/", include(("lettings.urls", "lettings"), namespace="lettings")),
