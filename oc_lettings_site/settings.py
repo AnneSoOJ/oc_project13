064e9cd448f25d817d25a62a3c03c540ca8d9536
AnneSoOJ <annesophie.ombouajalais@jask-si.com>
2022-12-25 22:45:46 +0100

    CI/CD pipeline

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,10 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    'localhost:8000',
+    ]
 
 
 # Application definition
